ebeaa565a414b077664edd06828ce2c6662c9529
itinajero <ivanetinajero@gmail.com>
2017-03-31 14:23:59 -0600

    Se actualiza BD Final

--- curso-cozcyt/capacitacion.sql
+++ curso-cozcyt/capacitacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 28, 2017 at 03:31 PM
+-- Generation Time: Mar 31, 2017 at 03:23 PM
 -- Server version: 5.7.17
 -- PHP Version: 7.0.16-4+deb.sury.org~trusty+1
 
@@ -37,9 +37,10 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(4, '2014_10_12_000000_create_users_table', 1),
-(5, '2014_10_12_100000_create_password_resets_table', 1),
-(6, '2017_03_28_155517_create_posts_table', 1);
+(7, '2014_10_12_000000_create_users_table', 1),
+(8, '2014_10_12_100000_create_password_resets_table', 1),
+(9, '2017_03_28_155517_create_posts_table', 1),
+(10, '2017_03_30_180619_add_userid_to_posts_table', 1);
 
 -- --------------------------------------------------------
 
@@ -65,18 +66,19 @@ CREATE TABLE `posts` (
   `contenido` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `activo` tinyint(1) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id`, `titulo`, `contenido`, `activo`, `created_at`, `updated_at`) VALUES
-(2, 'Post 2', 'Contenido del post 2', 0, NULL, NULL),
-(3, 'Post 3', 'Contenido del post 3', 1, NULL, NULL),
-(5, 'Post 5', 'Contenido del post 5', 1, NULL, NULL),
-(6, 'Post 6', 'Contenido del post 6', 1, '2017-03-29 00:15:07', '2017-03-29 00:38:31');
+INSERT INTO `posts` (`id`, `titulo`, `contenido`, `activo`, `created_at`, `updated_at`, `user_id`) VALUES
+(1, 'Manual de Javadd', 'Manual de JSF', 1, '2017-03-31 00:29:56', '2017-04-01 00:57:35', 1),
+(2, 'Curso de Angular 1', 'el contenido', 1, '2017-03-31 00:31:11', '2017-04-01 00:58:04', 1),
+(3, 'Curso de Angular', 'el contenido', 1, '2017-03-31 00:32:07', '2017-03-31 00:32:07', 1),
+(4, 'Mi primer con mensaje', 'Mi  contenido', 0, '2017-04-01 02:07:43', '2017-04-01 02:08:00', 1);
 
 -- --------------------------------------------------------
 
@@ -93,6 +95,14 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Ivan Eliseo Tinajero Diaz', 'ivanetinajero@gmail.com', '$2y$10$dbNFpmAPuR/TNM.3kha4xOSWjGAHvFZ/m0NjzzUP.lkZS7HOMGLna', '2JXdIM198XAH3vyy3vwoC4fGHYLZGOzYnQFbHopNJaqJ3rlGUD2dcfk6t7QX', '2017-03-31 00:24:09', '2017-03-31 00:24:09'),
+(2, 'Maria Lopez', 'maria@test.com', '$2y$10$9gqxhDbFUjtAJ.SHRt6IVuvyNRhnDnqIV1NwOFSqySnXBC3e77vTS', NULL, '2017-03-31 00:39:28', '2017-03-31 00:39:28');
 
 --
 -- Indexes for dumped tables
@@ -114,7 +124,8 @@ ALTER TABLE `password_resets`
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `posts_user_id_foreign` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -131,17 +142,27 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
